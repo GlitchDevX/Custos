55c30468049bbed21ac79fd64da5ff66dfd62bd6
kjansen <kai.jansen@otto.de>
2025-03-26 17:18:46 +0100

    Added Backend structure

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+import argparse
+from app import create_app
+from app.config import DevelopmentConfig, TestingConfig, ProductionConfig
+
+configs = {
+    "dev": DevelopmentConfig,
+    "tst": TestingConfig,
+    "prd": ProductionConfig
+}
+
+def run(config):
+    app = create_app(config)
+    app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run the Flask application.')
+    parser.add_argument('--config', type=str, choices=['dev', 'tst', 'prd'],
+                        default='development', help='Choose the configuration to use')
+
+    args = parser.parse_args()
+    run(configs.get(args.config))
