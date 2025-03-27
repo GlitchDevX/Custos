74f376aa8dcd10a858f4e6abcd13467894a66eb2
kjansen <kai.jansen@otto.de>
2025-03-27 13:12:06 +0100

    Added test

--- backend/tests/test_mail_endpoint.py
+++ backend/tests/test_mail_endpoint.py
@@ -1,3 +1,7 @@
 def test_should_throw_bad_request_on_wrong_body(client):
     response = client.post("/validate-mail/", json={"mal": "test"})
     assert response.status_code == 400
+
+def test_should_return_ok_status_code(client):
+    response = client.post("/validate-mail/", json={"mail": "test"})
+    assert response.status_code == 200
