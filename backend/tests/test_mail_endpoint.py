7f9d9b3246b60479e3649152681e76493096216e
JonasRingeis <jonas.ringeis@otto.de>
2025-03-27 17:46:10 +0100

    Updated tests to reflect response type change

--- backend/tests/test_mail_endpoint.py
+++ backend/tests/test_mail_endpoint.py
@@ -1,3 +1,6 @@
+from flask import json
+
+
 def test_should_throw_bad_request_on_wrong_body(client):
     response = client.post("/validate-mail/", json={"mal": "test"})
     assert response.status_code == 400
@@ -5,18 +8,19 @@ def test_should_throw_bad_request_on_wrong_body(client):
 def test_should_return_ok_status_code(client):
     response = client.post("/validate-mail/", json={"mail": "test@gmail.com"})
     assert response.status_code == 200
+    assert json.loads(response.json)["code"] == "OK"
 
 def test_should_get_invalid_format(client):
     response = client.post("/validate-mail/", json={"mail": "invalid-mail-format"})
     assert response.status_code == 400
-    assert response.json == "FORMAT_INVALID"
+    assert json.loads(response.json)["code"] == "FORMAT_INVALID"
 
 def test_should_get_disposable_mail(client):
     response = client.post("/validate-mail/", json={"mail": "test@muellmail.com"})
     assert response.status_code == 400
-    assert response.json == "DISPOSABLE"
+    assert json.loads(response.json)["code"] == "DISPOSABLE"
 
 def test_should_get_no_mailserver(client):
     response = client.post("/validate-mail/", json={"mail": "test@1.1.1.1"}) # 1.1.1.1 has no MX-Records set
     assert response.status_code == 400
-    assert response.json == "NO_MAILSERVER"
+    assert json.loads(response.json)["code"] == "NO_MAILSERVER"
