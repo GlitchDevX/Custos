f084605fb56eb0487368d7c09b5987e564cd4e23
JonasRingeis <jonas.ringeis@otto.de>
2025-03-28 12:25:32 +0100

    Got mail server validator working

--- backend/app/services/mail_validation/mailserver_validator.py
+++ backend/app/services/mail_validation/mailserver_validator.py
@@ -1,9 +1,75 @@
+import smtplib
+import socket
+from typing import Set
+import dns.resolver
+
 from .validation_result import ValidationResult
 from .validator_module import ValidatorModule
 
-
 class MailserverValidator(ValidatorModule):
 
+    _successful_cache: Set[str] = set()
+
+    _no_mailserver = ValidationResult(False, "NO_MAILSERVER", "The Mailserver is not reachable")
+    _ok_result = ValidationResult(True, "OK", "Everything okay")
+
+    def _get_domain_from_email(_, email: str):
+        return email[email.find('@')+1:]
+    
+    def _get_mail_servers(_, domain):
+        mail_servers = dns.resolver.resolve(domain, 'MX', raise_on_no_answer=False)
+        servers_dict = {}
+        for server in mail_servers:
+            priority, host = server.to_text().split(' ')
+            servers_dict[host] = int(priority)
+        
+        sorted_servers_tuples = sorted(servers_dict.items(), key=lambda x: x[1])
+        sorted_servers = list(map(lambda tuple: tuple[0], sorted_servers_tuples))
+
+        return sorted_servers
+
+    def _check_mail_servers(_, mail_host: str, email: str):
+        print(f"Will check mail server with host: {mail_host}")
+        
+        try:
+            server = smtplib.SMTP()
+            server.set_debuglevel(0)
+
+            server.connect(mail_host)
+            server.helo("custus.com")
+            server.mail("me@custos.com")
+            code, message = server.rcpt(email)
+            server.quit()
+        
+            print(f"Got Code {code} when testing mail host: {mail_host}. Response Message: '{message.decode()}'")
+            return code == 250
+        
+        except smtplib.SMTPServerDisconnected as e:
+            pass
+            # save metric
+        except smtplib.SMTPConnectError as e:
+            pass
+            # save metric
+        except socket.timeout as e:
+            pass
+            # save metric
+
+
     def execute_check(self, email):
-        # impl mailserver check via dns record domain
-        return ValidationResult(False, "NO_MAILSERVER", "The Mailserver is not reachable")
+        domain = self._get_domain_from_email(email)
+        
+        servers = self._get_mail_servers(domain)
+        if len(servers) == 0:
+            return self._no_mailserver
+        
+        if email in self._successful_cache:
+            return self._ok_result
+        
+        valid = False
+        for server in servers:
+            valid = self._check_mail_servers(server, email)
+            if valid:
+                self._successful_cache.add(email)
+                break
+        
+        return self._ok_result if valid else self._no_mailserver
