4fc805163cc3a4bda6ed48f34406aa1eef24371d
JonasRingeis <jonas.ringeis@otto.de>
2025-03-31 09:49:25 +0200

    Updated mail metric field names

--- backend/app/models/email_metrics.py
+++ backend/app/models/email_metrics.py
@@ -7,6 +7,6 @@ class EmailMetrics(db.Model, BaseModel):
     __tablename__ = 'metrics'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    invalid_format = db.Column(db.Boolean, nullable=True)
-    no_dns_record = db.Column(db.Boolean, nullable=True)
-    spam_mail = db.Column(db.Boolean, nullable=True)
+    invalid_format = db.Column(db.Boolean, nullable=True, default=None)
+    disposable = db.Column(db.Boolean, nullable=True, default=None)
+    no_mailserver = db.Column(db.Boolean, nullable=True, default=None)
