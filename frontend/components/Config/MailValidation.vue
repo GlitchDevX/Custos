f7e00a6cbe76359be57dd4d456cd7a473e5ab18b
JonasRingeis <jonas.ringeis@gmail.com>
2025-03-31 20:36:59 +0200

    Got frontend loading config working

--- frontend/components/Config/MailValidation.vue
+++ frontend/components/Config/MailValidation.vue
@@ -3,60 +3,63 @@
     <EndpointSummary path="/validate-mail" title="Mail Validation"
         summary="This endpoint is used to verify if an email is valid by using different techniques."
     />
-    <USwitch
-        unchecked-icon="lucide-x"
-        checked-icon="lucide-check"
-        label="Endpoint enabled"
-        size="xl"
-        class="pb-8"
-        v-model="state.enabled"
-    />
-
-    <div :class="{'low-opacity': !state.enabled}">
-        <FeatureToggle
-            title="Format Check"
-            description="Apply basic format checks to an email."
-            v-model="state.formatCheck"
-        />
-        <FeatureToggle
-            title="Disposable Check"
-            description="Check the email for common disposable domains."
-            v-model="state.disposableCheck"
+    <Loader v-if="loading" label="Loading Config"/>
+    <div v-show="loaded">
+        <USwitch
+            unchecked-icon="lucide-x"
+            checked-icon="lucide-check"
+            label="Endpoint enabled"
+            size="xl"
+            class="pb-8"
+            v-model="state.enabled"
         />
 
-        <UFormField label="Extra Disposable Domains" size="xl" class="pt-4" :class="{'low-opacity': !state.disposableCheck}">
-            <p class="muted-text">
-                Comma separated list of additional domains to mark as disposable.
-            </p>
-            <UTextarea placeholder="gmail.com, github.com..."
-                :maxrows="5" :autoresize="true" class="w-116 mt-1" />
-        </UFormField>
+        <div :class="{'low-opacity': !state.enabled}">
+            <FeatureToggle
+                title="Format Check"
+                description="Apply basic format checks to an email."
+                v-model="state.formatCheck"
+            />
+            <FeatureToggle
+                title="Disposable Check"
+                description="Check the email for common disposable domains."
+                v-model="state.disposableCheck"
+            />
 
-        <FeatureToggle
-            title="MX Record Check"
-            description="Checks if the domain has a mail server in the MX Records."
-            v-model="state.mxRecordCheck" @update:model-value="onMxRecordChange"
-        />
-        <FeatureToggle
-            title="SMTP HELO Check"
-            description="Perform a SMTP HELO check verifying the existance of the mail server."
-            v-model="state.smtpHelo" @update:model-value="onSmtpHeloChange"
-        />
-        <UFormField label="Max Mailserver Checks" size="xl" class="pt-4" :class="{'low-opacity': !state.smtpHelo}">
-            <p class="muted-text">
-                Maximum amount of mail servers to perform a HELO check.
-            </p>
-            <UInputNumber v-model="state.maxHeloChecks" class="w-32 mt-1" :min="1" />
-        </UFormField>
+            <UFormField label="Extra Disposable Domains" size="xl" class="pt-4" :class="{'low-opacity': !state.disposableCheck}">
+                <p class="muted-text">
+                    Comma separated list of additional domains to mark as disposable.
+                </p>
+                <UTextarea placeholder="gmail.com, github.com..."
+                    :maxrows="5" :autoresize="true" class="w-116 mt-1" />
+            </UFormField>
+
+            <FeatureToggle
+                title="MX Record Check"
+                description="Checks if the domain has a mail server in the MX Records."
+                v-model="state.mxRecordCheck" @update:model-value="onMxRecordChange"
+            />
+            <FeatureToggle
+                title="SMTP HELO Check"
+                description="Perform a SMTP HELO check verifying the existance of the mail server."
+                v-model="state.smtpHelo" @update:model-value="onSmtpHeloChange"
+            />
+            <UFormField label="Max Mailserver Checks" size="xl" class="pt-4" :class="{'low-opacity': !state.smtpHelo}">
+                <p class="muted-text">
+                    Maximum amount of mail servers to perform a HELO check.
+                </p>
+                <UInputNumber v-model="state.maxHeloChecks" class="w-32 mt-1" :min="1" />
+            </UFormField>
+        </div>
+
+        <UButton label="Save" size="lg" class="mt-8"
+            @click="submitConfig" :loading="sending"/>
     </div>
-
-    <UButton label="Save" size="lg" class="mt-8"
-        @click="submitConfig" :loading="sending"/>
-  </ConfigLayout>
+</ConfigLayout>
 </template>
 
 <script lang="ts" setup>
-import { SET_CONFIG_PATH } from '~/assets/ts/backendConnector';
+import { CONFIG_PATH } from '~/assets/ts/backendConnector';
 
 const toast = useToast();
 const state = reactive({
@@ -80,6 +83,28 @@ function onSmtpHeloChange() {
     }
 }
 
+const loading = ref(true);
+const loaded = ref(false);
+async function loadConfig() {
+    let result = {};
+    loading.value = true;
+    loaded.value = false;
+    try {
+        result = await $fetch<Object>(CONFIG_PATH + "?namespace=mail_validation", { method: 'GET' });
+        Object.assign(state, result);
+        loaded.value = true;
+        console.log(result);
+        
+    } catch {}
+
+    if (Object.keys(result).length === 0) {
+        showFail("Failed to get config from the backend", loadConfig);
+    }
+
+    loading.value = false;
+}
+loadConfig();
+
 const sending = ref(false);
 async function submitConfig() {
     const body = { 'namespace': 'mail_validation', 'content': state };
@@ -87,7 +112,7 @@ async function submitConfig() {
     
     let result: { code: string };
     try {
-        result = await $fetch<{code: string}>(SET_CONFIG_PATH, { method: 'POST', body: body, ignoreResponseError: true });
+        result = await $fetch<{code: string}>(CONFIG_PATH, { method: 'POST', body: body, ignoreResponseError: true });
     }
     catch {
         result = {code: "FAILED"};
@@ -97,7 +122,7 @@ async function submitConfig() {
         showSuccess();
     }
     else {
-        showFail();
+        showFail("Failed to update config in the backend.", submitConfig);
     }
     sending.value = false;
 }
@@ -110,10 +135,10 @@ function showSuccess() {
         color: 'success'
     });
 }
-function showFail() {
+function showFail(description: string, retryCallback: () => void) {
     toast.add({
         title: 'Failed',
-        description: 'Failed to update config in the backend.',
+        description: description,
         icon: 'lucide-x',
         color: 'error',
         duration: 8000,
@@ -123,9 +148,7 @@ function showFail() {
                 label: 'retry',
                 variant: 'outline',
                 color: 'neutral',
-                onClick: () => {
-                    submitConfig();
-                }
+                onClick: retryCallback
             }
         ],
     });
---
+++ frontend/components/Loader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-row items-center gap-2 py-4 px-2">
+    <div class="loader-spinner"></div>
+    <span>{{ props.label }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    label: String
+})
+</script>
+
+<style scoped>
+.loader-spinner {
+    border: 4px solid var(--ui-bg-accented);
+    border-top: 4px solid var(--ui-primary);
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+}
+@keyframes spin {
+    to {
+        rotate: 360deg;
+    }
+}
+</style>
