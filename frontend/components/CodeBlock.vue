40490f8375532a3ea5486e04c78eeb950910fdd2
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-01 17:44:20 +0200

    #minor feat: endpoint playground

---
+++ frontend/components/CodeBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="codeblock-wrapper p-4 rounded-lg">
+    <pre class="text-sm">{{ props.content }}</pre>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    content: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+.codeblock-wrapper {
+    background: var(--ui-color-neutral-800);
+}
+</style>
