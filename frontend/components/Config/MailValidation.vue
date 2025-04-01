3a1b05f35b278d82476d1875de1b57d119b784d7
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-01 09:15:10 +0200

    Got config loading in ui working

--- frontend/components/Config/MailValidation.vue
+++ frontend/components/Config/MailValidation.vue
@@ -1,48 +1,63 @@
 <template>
   <ConfigLayout>
-    <EndpointSummary path="/validate-mail" title="Mail Validation"
+    <EndpointSummary
+        path="/validate-mail" title="Mail Validation"
         summary="This endpoint is used to verify if an email is valid by using different techniques."
     />
-    <Loader v-if="loading" label="Loading Config"/>
+    <Spinner v-if="loading" label="Loading Config"/>
+    <UAlert
+        v-if="loadingFailed"
+        color="error" title="Failed to load Config"
+        icon="lucide-triangle-alert"
+        :actions="[{
+            icon: 'lucide-refresh-cw',
+            label: 'retry',
+            variant: 'soft',
+            color: 'neutral',
+            onClick: loadConfig
+        }]" />
     <div v-show="loaded">
         <USwitch
+            v-model="state.enabled"
             unchecked-icon="lucide-x"
             checked-icon="lucide-check"
             label="Endpoint enabled"
             size="xl"
             class="pb-8"
-            v-model="state.enabled"
         />
 
         <div :class="{'low-opacity': !state.enabled}">
             <FeatureToggle
+                v-model="state.formatCheck"
                 title="Format Check"
                 description="Apply basic format checks to an email."
-                v-model="state.formatCheck"
             />
             <FeatureToggle
+                v-model="state.disposableCheck"
                 title="Disposable Check"
                 description="Check the email for common disposable domains."
-                v-model="state.disposableCheck"
             />
 
             <UFormField label="Extra Disposable Domains" size="xl" class="pt-4" :class="{'low-opacity': !state.disposableCheck}">
                 <p class="muted-text">
                     Comma separated list of additional domains to mark as disposable.
                 </p>
-                <UTextarea placeholder="gmail.com, github.com..."
+                <UTextarea
+                    placeholder="gmail.com, github.com..."
                     :maxrows="5" :autoresize="true" class="w-116 mt-1" />
             </UFormField>
 
             <FeatureToggle
+                v-model="state.mxRecordCheck"
                 title="MX Record Check"
                 description="Checks if the domain has a mail server in the MX Records."
-                v-model="state.mxRecordCheck" @update:model-value="onMxRecordChange"
+                @update:model-value="onMxRecordChange"
             />
             <FeatureToggle
+                v-model="state.smtpHelo"
                 title="SMTP HELO Check"
                 description="Perform a SMTP HELO check verifying the existance of the mail server."
-                v-model="state.smtpHelo" @update:model-value="onSmtpHeloChange"
+                @update:model-value="onSmtpHeloChange"
             />
             <UFormField label="Max Mailserver Checks" size="xl" class="pt-4" :class="{'low-opacity': !state.smtpHelo}">
                 <p class="muted-text">
@@ -52,8 +67,11 @@
             </UFormField>
         </div>
 
-        <UButton label="Save" size="lg" class="mt-8"
-            @click="submitConfig" :loading="sending"/>
+        <UButton
+            label="Save" size="lg" class="mt-8"
+            :loading="sending"
+            @click="submitConfig"
+        />
     </div>
 </ConfigLayout>
 </template>
@@ -85,17 +103,21 @@ function onSmtpHeloChange() {
 
 const loading = ref(true);
 const loaded = ref(false);
+const loadingFailed = ref(false);
 async function loadConfig() {
     let result = {};
     loading.value = true;
     loaded.value = false;
+    loadingFailed.value = false;
     try {
         result = await $fetch<object>(CONFIG_PATH + "?namespace=mail_validation", { method: 'GET' });
         Object.assign(state, result);
         loaded.value = true;
         console.log(result);
         
-    } catch { /* empty */ }
+    } catch {
+        loadingFailed.value = true
+    }
 
     if (Object.keys(result).length === 0) {
         showFail("Failed to get config from the backend", loadConfig);
