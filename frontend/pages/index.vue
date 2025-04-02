a706f240cfc1b72ce65357f4f0dc8f92398c6bfb
GlitchDev <jonas.ringeis@gmail.com>
2025-04-02 23:07:57 +0200

    Moved mock data to flagged-users file

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -19,12 +19,6 @@
                 </li>
             </ul>
         </UCard>
-        <UCard>
-            <h2 class="text-2xl font-bold mb-4">
-                Flagged User Inbox
-            </h2>
-            <UTable :data="mockData"/>
-        </UCard>
     </div>
 </template>
 
@@ -35,84 +29,6 @@ import { TITLE_SUFFIX } from '~/assets/data/appData';
 useHead({
     title: 'Home' + TITLE_SUFFIX
 })
-
-const mockData = reactive([
-    {
-        id: 1,
-        username: "GlitchDev",
-        flags: ['slurs'],
-    },
-    {
-        "id": 2,
-        "username": "CodeNinja",
-        "flags": ["spam", "harassment"]
-    },
-    {
-        "id": 3,
-        "username": "TechGuru",
-        "flags": ["slurs"]
-    },
-    {
-        "id": 4,
-        "username": "User123",
-        "flags": ["hate speech", "misinformation"]
-    },
-    {
-        "id": 5,
-        "username": "DevMaster",
-        "flags": ["spam"]
-    },
-    {
-        "id": 6,
-        "username": "PixelArtist",
-        "flags": ["harassment"]
-    },
-    {
-        "id": 7,
-        "username": "GamerPro",
-        "flags": ["slurs", "hate speech"]
-    },
-    {
-        "id": 8,
-        "username": "WebWizard",
-        "flags": ["misinformation"]
-    },
-    {
-        "id": 9,
-        "username": "DataDynamo",
-        "flags": ["spam", "harassment"]
-    },
-    {
-        "id": 10,
-        "username": "CryptoQueen",
-        "flags": ["scams"]
-    },
-    {
-        "id": 11,
-        "username": "DesignDiva",
-        "flags": ["slurs"]
-    },
-    {
-        "id": 12,
-        "username": "MarketingMaven",
-        "flags": ["misinformation", "spam"]
-    },
-    {
-        "id": 13,
-        "username": "AIWhiz",
-        "flags": ["hate speech"]
-    },
-    {
-        "id": 14,
-        "username": "GameChanger",
-        "flags": ["harassment", "slurs"]
-    },
-    {
-        "id": 15,
-        "username": "CodeCrafter",
-        "flags": ["spam"]
-    },
-])
 </script>
 
 <style scoped>
