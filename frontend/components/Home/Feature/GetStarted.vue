35330574409e408459f2e53c64d0e37b838599ee
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-03 15:33:37 +0200

    Built homepage

---
+++ frontend/components/Home/Feature/GetStarted.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="get-started" class="flex justify-center items-center">
+        <div class="w-3xl">
+            <div class="max-w-xl">
+                <h2 class="text-3xl font-bold">
+                    Start with Docker Compose
+                </h2>
+                <p class="muted-text">
+                    You can start off by simply creating a new directory with a docker-compose.yaml and this content.
+                </p>
+            </div>
+            <CodeBlock :content="compose" :showCopy="true" class="mt-4" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import compose from '~/assets/data/docker-compose.yaml?raw';
+
+</script>
+
+<style>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,34 +1,19 @@
 <template>
     <div class="p-4 flex gap-4 flex-col">
-        <UCard>
-            <h1 class="text-2xl font-bold mb-4">
-                UI Elements
-            </h1>
-            <ul>
-                <li>
-                    Blacklist editor
-                </li>
-                <li>
-                    Flagged User Inbox
-                </li>
-                <li>
-                    Test Endpoints
-                </li>
-                <li>
-                    Config UI Editor (After MVP)
-                </li>
-            </ul>
-        </UCard>
+        <HomeHero />
+        <HomeFeatureMailValidation class="pt-8" />
+        <HomeFeatureRealtimeContent class="pt-8" />
+        <HomeFeaturePipeline class="pt-8" />
+        <HomeFeatureGetStarted class="pt-8" />
     </div>
 </template>
 
 <script lang="ts" setup>
-import { UTable } from '#components';
 import { TITLE_SUFFIX } from '~/assets/data/appData';
 
 useHead({
     title: 'Home' + TITLE_SUFFIX
-})
+});
 </script>
 
 <style scoped>
