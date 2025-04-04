2557696888cd827913ee0925d013d1be6e968357
GlitchDev <jonas.ringeis@gmail.com>
2025-04-04 16:20:39 +0200

    Implemented report endpoint for pipeline to read from

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,6 +5,7 @@ from .resources.metrics import ns_metric
 from .resources.mail_adress import ns_mail
 from .resources.config import ns_config
 from .resources.content_check import ns_content_check
+from .resources.report_content import ns_report
 from .utils.sqlalchemy_utils import SQLAlchemySingleton
 from .models.metric import Metric
 
@@ -33,6 +34,7 @@ class FlaskApplication:
         api.add_namespace(ns_mail)
         api.add_namespace(ns_config)
         api.add_namespace(ns_content_check)
+        api.add_namespace(ns_report)
 
         self.db.init_app(self.flask_app)
 
---
+++ backend/app/services/deep_check/pipeline_submitter.py
@@ -0,0 +1,19 @@
+import uuid
+
+from app.models.reported_content import ReportedContent
+
+from ...utils.sqlalchemy_utils import SQLAlchemySingleton
+
+
+db = SQLAlchemySingleton()
+
+class PipelineSubmitter:
+    
+    def submit(self, userId, content):
+        report = ReportedContent()
+        report.report_id = uuid.uuid4()
+        report.user_id = userId
+        report.content = content
+
+        db.session.add(report)
+        db.session.commit()
