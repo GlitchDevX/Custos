9c628bdd1a3f7cb2d911f6fa41a80fdc3154ea90
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-07 15:08:35 +0200

    Added reportedAt to report data

--- backend/app/models/flagged_content.py
+++ backend/app/models/flagged_content.py
@@ -2,10 +2,11 @@ from ..utils.sqlalchemy_utils import SQLAlchemySingleton
 
 db = SQLAlchemySingleton()
 
-class ReportedContent(db.Model):
+class FlaggedContent(db.Model):
     __tablename__ = 'flagged_content'
     
     report_id = db.Column(db.String(50), primary_key=True)
+    reported_at = db.Column(db.DateTime())
     user_id = db.Column(db.String(100))
     content = db.Column(db.String(5000))
     flags = db.Column(db.String(200)) # comma separated list
--- backend/app/models/reported_content.py
+++ backend/app/models/reported_content.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from ..utils.sqlalchemy_utils import SQLAlchemySingleton
 
 db = SQLAlchemySingleton()
@@ -6,5 +7,6 @@ class ReportedContent(db.Model):
     __tablename__ = 'reported_content'
     
     report_id = db.Column(db.String(50), primary_key=True)
+    reported_at = db.Column(db.DateTime, default=datetime.now())
     user_id = db.Column(db.String(100))
     content = db.Column(db.String(5000))
