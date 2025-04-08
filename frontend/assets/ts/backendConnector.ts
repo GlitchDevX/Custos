c01e61cd215dd60f0194008fcf17ee3603e79705
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-08 20:43:30 +0200

    Added pipeline path to frontend

--- frontend/assets/ts/backendConnector.ts
+++ frontend/assets/ts/backendConnector.ts
@@ -4,3 +4,4 @@ export const VALIDATE_MAIL_PATH = BASE_PATH + "/validate-mail/";
 export const REALTIME_CONTENT_CHECK_PATH = BASE_PATH + "/check-content/";
 export const CONFIG_PATH = BASE_PATH + "/config/";
 export const METRIC_PATH = BASE_PATH + "/metrics/";
+export const PIPELINE_PATH = BASE_PATH + "/pipeline/";
