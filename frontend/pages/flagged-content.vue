22b592d8bee7734eea17efbaa2ae1180f345188e
JonasRingeis <jonas.ringeis@gmail.com>
2025-04-09 09:45:28 +0200

    Added pipeline status card

--- frontend/pages/flagged-content.vue
+++ frontend/pages/flagged-content.vue
@@ -3,20 +3,49 @@
     <PageTitle>
       Flagged Content Inbox
     </PageTitle>
-    <!-- <UTable :data="mockData" :columns="columns" :sticky="true" /> -->
-    <UTable :data="data" :columns="columns" :sticky="true" :loading="loading" />
+    <UTable :data="data" :columns="columns" :loading="loading" />
+    <UCard>
+      <div class="flex gap-2 items-center">
+        <h2 class="text-2xl font-bold" >
+          Pipeline Status
+        </h2>
+        <UBadge v-if="status.active" label="Active" icon="lucide-circle-dot" color="success" />
+        <UBadge v-else label="Inactive" icon="lucide-circle-dot" color="error" />
+      </div>
+      
+      <UProgress v-model="status.processed" :max="status.total" class="mt-4" />
+      
+      <span class="text-2xl font-bold mt-8">
+        {{ status.processed }} / {{ status.total }}
+      </span>
+    </UCard>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { TableColumn } from '@nuxt/ui';
 import { TITLE_SUFFIX } from '~/assets/data/appData';
-import { FLAGGED_CONTENT_PATH } from '~/assets/ts/backendConnector';
+import { FLAGGED_CONTENT_PATH, PIPELINE_PATH } from '~/assets/ts/backendConnector';
 import type { FlaggedUser as FlaggedContent } from '~/assets/types/flaggedContent';
 
 onMounted(() => {
   loadData();
 });
+
+
+const status = reactive({
+	active: false,
+	total: 0,
+	processed: 0,
+	ratio: 0.0
+});
+async function loadStatus() {
+  status = await $fetch<any>(PIPELINE_PATH);
+}
+
+onMounted(() => {
+  loadStatus();
+})
 
 const data = ref<FlaggedContent[]>([]);
 const loading = ref(true);
