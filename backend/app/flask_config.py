acc29034f9c96d0838a1f33c59d5a42a9cbdd45a
GlitchDev1 <jonas.ringeis@gmail.com>
2025-04-13 13:30:43 +0200

    #minor updated ports for whole application

--- backend/app/flask_config.py
+++ backend/app/flask_config.py
@@ -3,14 +3,14 @@ from secrets import token_urlsafe
 
 class FlaskBaseConfig:
     HOST = "0.0.0.0"
-    PORT = 5000
+    PORT = 5365
     SECRET_KEY = os.environ.get('SECRET_KEY', token_urlsafe(32))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     DEBUG = False
 
 class FlaskDevConfig(FlaskBaseConfig):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost/custos"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost:5385/custos"
     DEBUG = True
 
 class FlaskTstConfig(FlaskBaseConfig):
--- frontend/assets/ts/backendConnector.ts
+++ frontend/assets/ts/backendConnector.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://localhost:5000";
+export const BASE_PATH = "http://localhost:5365";
 
 export const VALIDATE_MAIL_PATH = BASE_PATH + "/mail/";
 export const REALTIME_CONTENT_CHECK_PATH = BASE_PATH + "/content/";
@@ -7,5 +7,3 @@ export const METRIC_PATH = BASE_PATH + "/metrics/";
 export const PIPELINE_PATH = BASE_PATH + "/pipeline/";
 export const REPORT_CONTENT_PATH = BASE_PATH + "/report-content/";
 export const FLAGGED_CONTENT_PATH = BASE_PATH + "/flagged-content/";
-
-//TODO: Add ui to submit report and trigger pipeline
