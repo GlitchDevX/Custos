1a0b4b446af737d8cd3390a29936f71183d3c2e0
GlitchDev <jonas.ringeis@gmail.com>
2025-04-18 01:10:00 +0200

    #minor added basic pipeline scheduling

--- backend/app/services/deep_check/pipeline_submitter.py
+++ backend/app/services/deep_check/pipeline_submitter.py
@@ -1,3 +1,4 @@
+import atexit
 from threading import Thread
 from app.models.reported_content import ReportedContent
 from app.utils.singleton_meta import SingletonMeta
@@ -6,12 +7,18 @@ from pipeline.pipeline import Pipeline
 from app.config_reader import ConfigReader
 from app.utils.common_responses import ENDPOINT_DISABLED
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 class PipelineSubmitter(metaclass=SingletonMeta):
 
     def __init__(self):
-        self.config = ConfigReader("pipeline")
+        self.config = ConfigReader("pipeline", self.reschedule_pipeline)
         self.pipeline = Pipeline()
+
+        self.scheduler = BackgroundScheduler()
+        self.schedule_pipeline()
+        atexit.register(lambda: self.scheduler.shutdown())
 
     def run_pipeline(self):
         if not self.config.get("enabled"):
@@ -35,3 +42,12 @@ class PipelineSubmitter(metaclass=SingletonMeta):
 
     def get_status(self):
         return self.pipeline.get_status(), 200
+
+    def reschedule_pipeline(self):
+        self.scheduler.shutdown()
+        self.scheduler = BackgroundScheduler()
+        self.schedule_pipeline()
+
+    def schedule_pipeline(self):
+        self.scheduler.add_job(self.run_pipeline, 'interval', minutes=2)
+        self.scheduler.start()
