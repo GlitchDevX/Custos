ab3a3e07dc1b2f6d5cb2f5f1f66ecd18afc9cd01
GlitchDev <jonas.ringeis@gmail.com>
2025-04-18 01:07:02 +0200

    #minor added update callback to config reader

--- backend/app/config_reader.py
+++ backend/app/config_reader.py
@@ -1,5 +1,6 @@
 import json
-from typing import List
+from typing import List, Dict
+
 
 class ConfigReader:
     """
@@ -11,18 +12,23 @@ class ConfigReader:
 
     :param namespace (str): The namespace used to locate the configuration file.
     """
+    config: Dict
 
-    def __init__(self, namespace):
+    def __init__(self, namespace, update_callback = None):
         self.namespace = namespace
         self.read_conf_file()
+        self.update_callback = update_callback
         instances.append(self)
 
-    def read_conf_file(self):
+    def read_conf_file(self, give_callback=False):
         try:
             with open(f"config/{self.namespace}.json") as file:
                 self.config = json.loads(file.read())
+
+            if give_callback and self.update_callback is not None:
+                self.update_callback()
         except:
-            self.config = None
+            self.config = {}
 
     def get_all(self):
         return self.config
@@ -35,4 +41,4 @@ instances: List[ConfigReader] = []
 def reload_readers_with_namespace(namespace):
     for instance in instances:
         if instance.namespace == namespace:
-            instance.read_conf_file()
+            instance.read_conf_file(True)
