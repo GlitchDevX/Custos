7d503316a1ba36188a7b430278e77ef62766785a
GlitchDevX <jonas.ringeis@gmail.com>
2025-08-27 21:56:17 +0200

    removed all ollama references from backend

--- backend/app/config/app_config.py
+++ backend/app/config/app_config.py
@@ -2,11 +2,9 @@ import os
 
 class BaseConfig:
     DB_URI = os.environ.get("DATABASE_URI")
-    LLM_URI = "http://ollama:5375"
 
 class DevConfig(BaseConfig):
     DB_URI = "mysql+pymysql://user:password@localhost:5385/custos"
-    LLM_URI = "http://localhost:5375"
 
 class TstConfig(BaseConfig):
     DB_URI = "sqlite:///:memory:"
