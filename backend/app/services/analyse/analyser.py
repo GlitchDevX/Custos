a299fb27d3ab7b9a020317366bd3af350a98807d
GlitchDevX <jonas.ringeis@gmail.com>
2025-08-29 12:23:26 +0200

    added analyzer metrics

--- backend/app/services/analyse/analyser.py
+++ backend/app/services/analyse/analyser.py
@@ -1,6 +1,7 @@
 from detoxify import Detoxify
 
 from app.config_reader import ConfigReader
+from app.models.metric import Metric
 from app.utils.common_responses import ENDPOINT_DISABLED
 
 
@@ -20,6 +21,8 @@ class AnalyserService:
         if not self.config.get("enabled"):
             return ENDPOINT_DISABLED
 
+        Metric.increase("ANALYZER_EXECUTED")
+
         result = self.model.predict(content)
         excluded = self.config.get("labelsToExclude")
         threshold = self.config.get("threshold")
@@ -28,5 +31,8 @@ class AnalyserService:
         for key, value in result.items():
             if key not in excluded and value > threshold:
                 labels.append(key)
+
+        if len(labels) > 0:
+            Metric.increase("ANALYZER_TOXICITY_DETECTED")
 
         return {"labels": labels}
