cc2be900b3b7f78af50bf6b6c7cc7d9ca7914ac3
GlitchDevX <jonas.ringeis@gmail.com>
2025-08-29 14:17:19 +0200

    added analysis playground

--- frontend/assets/ts/backendConnector.ts
+++ frontend/assets/ts/backendConnector.ts
@@ -2,6 +2,7 @@ export const BASE_PATH = "http://localhost:5365";
 
 export const VALIDATE_MAIL_PATH = BASE_PATH + "/mail/";
 export const REALTIME_CONTENT_CHECK_PATH = BASE_PATH + "/content/";
+export const ANALYZE_CONTENT_PATH = BASE_PATH + "/analyze/";
 export const CONFIG_PATH = BASE_PATH + "/config/";
 export const METRIC_PATH = BASE_PATH + "/metrics/";
 
