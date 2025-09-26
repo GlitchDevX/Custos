82da75f469f6a7d861449d1c1db66bedb68c7307
GlitchDevX <jonas.ringeis@gmail.com>
2025-09-26 19:36:42 +0200

    Finished new metrics page

--- frontend/app/pages/metrics.vue
+++ frontend/app/pages/metrics.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-4 px-8 flex gap-4 flex-col">
-    <PageTitle>Custos Metrics</PageTitle>
+    <PageTitle>Custos Service Metrics</PageTitle>
     <p>
       Custos offers a prometheus exporter on the path
       <UBadge label="/metrics" variant="subtle" /> that you can scrape with your Prometheus instance.
@@ -14,12 +14,19 @@
         <p>
           Edit your prometheus config at
           <UBadge label="/etc/prometheus/prometheus.yml" color="neutral" variant="subtle" />
+          and add this scrape config:
         </p>
 
-        <CodeBlock class="mt-2" lang="yaml" :showCopy="true" content="scrape_configs:
-  - job_name: 'custos'
-    static_configs:
-      - targets: ['localhost:3060']  # assuming your Custos instance is running on port 3060" />
+        <CodeBlock class="mt-2" lang="yaml" :showCopy="true" :content="prometheusConfig" />
+      </template>
+      <template #default>
+        <h2 class="text-2xl font-bold">
+          Grafana Dashboard
+        </h2>
+        <p>
+          Create a new grafana dashboard from this json.
+        </p>
+        <CodeBlock class="mt-2 h-64" lang="json" :showCopy="true" :content="grafanaDashboard" />
       </template>
     </UCard>
 
@@ -34,6 +41,8 @@
 
 <script lang="ts" setup>
 import { TITLE_SUFFIX } from '~/assets/data/appData';
+import grafanaDashboard from '~/assets/data/metric-configs/grafana-dashboard.json?raw';
+import prometheusConfig from '~/assets/data/metric-configs/prometheus-config.yml?raw';
 
 const { getMetrics } = useBackend();
 
