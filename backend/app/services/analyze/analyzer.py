3399b9e079632041b328a0a4da43bbb3b83b4755
GlitchDevX <jonas.ringeis@gmail.com>
2025-10-07 22:10:57 +0200

    #minor made analyzer load detoxify on new thread to improve application startup speed

--- backend/app/services/analyze/analyzer.py
+++ backend/app/services/analyze/analyzer.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from app.utils.logger import logger
 from app.utils.singleton_meta import SingletonMeta
 from app.services.metrics.metrics_counter import count_metric
@@ -14,17 +15,31 @@ class AnalyzerService(metaclass=SingletonMeta):
     """
 
     config = ConfigReader("deep_analysis")
+    loading_thread: Thread
+    model: Detoxify | None = None
 
     def __init__(self):
+        self.loading_thread = Thread(target=self._load_model)
+        self.loading_thread.start()
+
+    def _load_model(self): 
         try:
             self.model = Detoxify("multilingual")
+            logger.info("Detoxify model loaded")
         except Exception as err:
-            logger.warning(f"Could not get detoxify model. {err}")
-
+            logger.warning(f"Could not get detoxify model. Error: {err}")
 
     def analyze_content(self, content: str):
         if not self.config.get("enabled"):
             return ENDPOINT_DISABLED
+        
+        if self.model is None:
+            if self.loading_thread.is_alive():
+                logger.warning("Could not analyze content, still waiting to load detoxify model")
+                return {'code': "MODEL_TEMPORARY_UNAVAILABLE", 'text': "The detoxify model is still loading, please try again in a minute."}, 503
+            else:
+                logger.error("Could not analyze content, detoxify model is missing")
+                return {'code': "MODEL_UNAVAILABLE", 'text': "The detoxify model is missing."}, 503
 
         count_metric('ANALYZER_EXECUTED')
 
