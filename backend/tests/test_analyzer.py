f985658c55e1f0d24d0dd2d1d07c7c21e349b914
GlitchDevX <jonas.ringeis@gmail.com>
2025-10-08 08:51:28 +0200

    #patch fixed analyzer test

--- backend/tests/test_analyzer.py
+++ backend/tests/test_analyzer.py
@@ -1,4 +1,18 @@
+from time import sleep
+
+import pytest
+
+from app.utils.logger import logger
+
 path = "/analyze/"
+
+@pytest.fixture(autouse=True)
+def around_tests(client):
+    backoff = 0.1
+    while client.post(path, json={"content": "x"}).json.get("code") == "MODEL_TEMPORARY_UNAVAILABLE":
+        sleep(backoff)
+        backoff += 0.1
+    yield
 
 def test_should_throw_bad_request_on_wrong_body(client):
     response = client.post(path, json={"invalid": "body"})
