f8d96e7c92ae3d526b53564f3264d370044f055f
GlitchDevX <jonas.ringeis@gmail.com>
2025-10-21 20:22:25 +0200

    Added grafana dashboard codeblock component to display dashboard code in docs

---
+++ frontend/app/components/content/GrafanaDashboardCode.vue
@@ -0,0 +1,16 @@
+<template>
+  <ProseCodeCollapse>
+    <ProsePre filename="dashboard.json" language="json">
+        {{ grafanaDashboard }}
+    </ProsePre>
+  </ProseCodeCollapse>
+</template>
+
+<script lang="ts" setup>
+import { ProseCodeCollapse } from '#components';
+import grafanaDashboard from '~/assets/data/metric-configs/grafana-dashboard.json?raw'
+</script>
+
+<style>
+
+</style>
