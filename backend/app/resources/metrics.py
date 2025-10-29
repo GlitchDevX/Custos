72280a799ccaff263a5f32c2bbcbf59870d7b6c1
GlitchDevX <jonas.ringeis@gmail.com>
2025-10-29 08:18:37 +0100

    Added documentation to metrics endpoint

--- backend/app/resources/metrics.py
+++ backend/app/resources/metrics.py
@@ -1,9 +1,12 @@
+from app.utils.common_responses import ENDPOINT_DISABLED, ENDPOINT_DISABLED_MODEL
 from flask import Response
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
 
 from app.services.metrics.metrics_reporter import MetricsReporter
 
 ns_metric = Namespace('metrics', description='Metrics')
+
+endpoint_disabled_model = ns_metric.model("endpoint_disabled", ENDPOINT_DISABLED_MODEL)
 
 @ns_metric.route('') # no trailing slash to match prometheus metrics path style
 class MetricsResource(Resource):
@@ -12,7 +15,14 @@ class MetricsResource(Resource):
     """
     metrics_reporter = MetricsReporter()
 
+    @ns_metric.response(200, 'Prometheus Metrics', fields.String())
+    @ns_metric.response(503, 'Endpoint Disabled', endpoint_disabled_model)
     def get(self):
+        """
+        Metrics
+
+        Returns service usage metrics as well as some generic flask metrics in the prometheus format.
+        """
         metrics = self.metrics_reporter.generate_metrics_report()
         if isinstance(metrics, tuple):
             return metrics
--- backend/app/utils/common_responses.py
+++ backend/app/utils/common_responses.py
@@ -1,1 +1,10 @@
-ENDPOINT_DISABLED = {'code': "ENDPOINT_DISABLED", 'text': "This endpoint is disabled. You can enable it in the config."}, 503
+from flask_restx import fields
+
+ENDPOINT_DISABLED = {
+    'code': "ENDPOINT_DISABLED",
+    'text': "This endpoint is disabled. You can enable it in the config."
+}, 503
+ENDPOINT_DISABLED_MODEL = {
+    'code': fields.String("ENDPOINT_DISABLED"),
+    'text': fields.String("This endpoint is disabled. You can enable it in the config.")
+}
