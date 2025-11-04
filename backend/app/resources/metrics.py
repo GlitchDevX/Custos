d8b00ed8abe6dc582b8a42a21a756755308faef7
GlitchDevX <jonas.ringeis@gmail.com>
2025-11-04 08:56:18 +0100

    improved documentation for metrics endpoint

--- backend/app/resources/metrics.py
+++ backend/app/resources/metrics.py
@@ -1,10 +1,10 @@
-from app.utils.common_responses import ENDPOINT_DISABLED, ENDPOINT_DISABLED_MODEL
 from flask import Response
 from flask_restx import Resource, Namespace, fields
 
 from app.services.metrics.metrics_reporter import MetricsReporter
+from app.utils.common_responses import ENDPOINT_DISABLED_MODEL
 
-ns_metric = Namespace('metrics', description='Metrics')
+ns_metric = Namespace('Metrics', description='Endpoint for getting metrics for Prometheus.', path='/metrics')
 
 endpoint_disabled_model = ns_metric.model("endpoint_disabled", ENDPOINT_DISABLED_MODEL)
 
@@ -19,9 +19,9 @@ class MetricsResource(Resource):
     @ns_metric.response(503, 'Endpoint Disabled', endpoint_disabled_model)
     def get(self):
         """
-        Metrics
+        /metrics
 
-        Returns service usage metrics as well as some generic flask metrics in the prometheus format.
+        Returns service usage metrics as well as some generic Flask metrics in the Prometheus format.
         """
         metrics = self.metrics_reporter.generate_metrics_report()
         if isinstance(metrics, tuple):
