9fbda20af6987a90fd5fd92f8d3b98e09b9dab41
GlitchDevX <jonas.ringeis@gmail.com>
2025-11-10 08:43:21 +0100

    Made endpoint disabled use error fieldname instead of code

--- backend/app/utils/common_responses.py
+++ backend/app/utils/common_responses.py
@@ -1,10 +1,10 @@
 from flask_restx import fields
 
 ENDPOINT_DISABLED = {
-    'code': "ENDPOINT_DISABLED",
+    'error': "ENDPOINT_DISABLED",
     'text': "This endpoint is disabled. You can enable it in the config."
 }, 503
 ENDPOINT_DISABLED_MODEL = {
-    'code': fields.String("ENDPOINT_DISABLED"),
+    'error': fields.String("ENDPOINT_DISABLED"),
     'text': fields.String("This endpoint is disabled. You can enable it in the config.")
 }
